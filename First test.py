079debdd5e90b41f45112c018ea7eaaf8f59d83b
Pear <phanlehoangxzc@gmail.com>
2026-06-06 21:55:05 +0700

    Test_1

--- First test.py
+++ First test.py
@@ -1,1 +1,3 @@
-print("Hello, world")
+character_name = input("What is your character's name?")
+character_age = input("What is your character's age?")
+print(f"Chào mừng {character_name} đến với thế giới của chúng tôi!")
