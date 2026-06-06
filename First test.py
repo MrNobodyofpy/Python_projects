bf552ea115a617c6817bfec514e57ffaf6001100
Pear <phanlehoangxzc@gmail.com>
2026-06-06 21:45:31 +0700

    Test_again

--- First test.py
+++ First test.py
@@ -1,8 +1,1 @@
-# Variable declarations
-name = "Alex"
-age = 20
-gpa = 3.5
-is_student = True
-
-print("Do you like Let the right one in?")
-response = input("Please answer with 'yes' or 'no': ")
+print("Hello, world")
