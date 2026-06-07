636a98134bd58373ae1d7393dfb402a54a91e97d
Pear <phanlehoangxzc@gmail.com>
2026-06-07 20:11:25 +0700

    BT1-Upgraded

--- BT1-Tong 2 so.py
+++ BT1-Tong 2 so.py
@@ -1,8 +1,24 @@
 #Import
 import math
 
-num1 = int(input("Enter a number: "))
-num2 = int(input("Enter another number: "))
+#Input
+num1 = float(input("Enter a number: "))
+num2 = float(input("Enter another number: "))
 
+#Sum of two number
 sum = num1 + num2
+
+#Xau du lieu
+
+## Xau hoa du lieu
+print("The first number is " + str(num1) + " and the second number is " + str(num2))
+
+## Formated sentences
+print(f"The first number is {num1} and the second number is {num2}")
+
+## Format and brackets
+text = "The first number is {0} and the second number is {1}"
+print(text.format(num1,num2))
+
+#Output
 print(f"The sum of those two numbers is: {sum}")
