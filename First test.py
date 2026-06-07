f2ee38089c3eb204db38a6deda7e5498c3354282
Pear <phanlehoangxzc@gmail.com>
2026-06-07 13:24:13 +0700

    Test_2

--- First test.py
+++ First test.py
@@ -1,3 +1,11 @@
-character_name = input("What is your character's name?")
-character_age = input("What is your character's age?")
-print(f"Chào mừng {character_name} đến với thế giới của chúng tôi!")
+character_name = "Petyr Baelish"
+character_age = 35
+
+is_smart = False
+if is_smart == False:
+    first_answer = "No"
+else:
+    first_answer = "Yes"
+
+print(f"His name is {character_name} and he is {character_age} years old.")
+print(f"Is he smart? {first_answer}")
