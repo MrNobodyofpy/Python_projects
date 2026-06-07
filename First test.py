6675df13e983d599dcd02912ee2cbfc1edc6519f
Pear <phanlehoangxzc@gmail.com>
2026-06-07 14:53:08 +0700

    First_steps

--- First test.py
+++ First test.py
@@ -7,5 +7,8 @@ if is_smart == False:
 else:
     first_answer = "Yes"
 
-print(f"His name is {character_name} and he is {character_age} years old.")
+info = f"His name is {character_name} and he is {character_age} years old."
+
+print(info + "\nWelcome new user!")
+
 print(f"Is he smart? {first_answer}")
