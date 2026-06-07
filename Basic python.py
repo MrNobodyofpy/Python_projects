328a563d55dc418e756b8db6f5cd29682d826048
Pear <phanlehoangxzc@gmail.com>
2026-06-07 17:04:58 +0700

    String_functions

--- Basic python.py
+++ Basic python.py
@@ -1,7 +1,7 @@
 character_name = "Petyr Baelish"
 character_age = 35
 
-is_smart = False
+is_smart = False 
 if is_smart == False:
     first_answer = "No"
 else:
@@ -12,3 +12,6 @@ info = f"His name is {character_name} and he is {character_age} years old."
 print(info + "\nWelcome new user!")
 
 print(f"Is he smart? {first_answer}")
+print(len(character_name))
+print(character_name.index("Baelish"))
+print(character_name[12])
