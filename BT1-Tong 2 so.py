df08521ea38019adeb0b56a604316e4bb9c9b363
Pear <phanlehoangxzc@gmail.com>
2026-06-09 13:28:45 +0700

    Function_upgraded

--- BT1-Tong 2 so.py
+++ BT1-Tong 2 so.py
@@ -5,20 +5,9 @@ import math
 num1 = float(input("Enter a number: "))
 num2 = float(input("Enter another number: "))
 
-#Sum of two number
-sum = num1 + num2
 
-#Xau du lieu
+def tinh_tong(so1,so2):
+    sum = num1 + num2
+    print(f"Tổng 2 số vừa nhập là: {sum}")
 
-## Xau hoa du lieu
-print("The first number is " + str(num1) + " and the second number is " + str(num2))
-
-## Formated sentences
-print(f"The first number is {num1} and the second number is {num2}")
-
-## Format and brackets
-text = "The first number is {0} and the second number is {1}"
-print(text.format(num1,num2))
-
-#Output
-print(f"The sum of those two numbers is: {sum}")
+tinh_tong(num1,num2)
--- BT2-List co ban.py
+++ BT2-List co ban.py
@@ -1,18 +1,16 @@
-import math
-list = input("Liệt kê 5 học sinh trong lớp của bạn " \
-"\nVui lòng nhập dưới dạng A,B,C,D,E ")
+#Lấy input
+hoc_sinh = input("Liệt kê 5 học sinh trong lớp của bạn \nVui lòng nhập dưới dạng A,B,C,D,E ").split(",")
 
-hoc_sinh = list.split(",")
+diem_so = input("Nhập lần lượt điểm của 5 học sinh đó với cú pháp tương tự: ").split(",")
 
-list2 = input("Nhập lần lượt điểm của 5 học sinh đó với cú pháp tương tự: ")
-diem_so = list2.split(",")
+hoc_sinh_changeable = hoc_sinh.copy()
 
 hoc_sinh.extend(diem_so)
 print(f"Các học sinh bạn nhập là: {hoc_sinh}, với phần sau là điểm tương ứng")
 
-list_moi = input("Nhập thêm 1 học sinh nữa:")
-hoc_sinh.append(list_moi)
+list_moi = input("Nhập thêm 1 học sinh nữa: ")
+hoc_sinh_changeable.append(list_moi)
 
-print(hoc_sinh)
+print(hoc_sinh_changeable)
 
 print(f"Số điểm 8 trong list điểm là: {diem_so.count("8")}")
